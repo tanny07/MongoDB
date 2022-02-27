cc332c58df07980fd798c255c69d768196388107
Tanamy Mondkar <tanmay-ankush.mondkar@epita.fr>
2022-02-27 18:55:42 +0100

    Update (MongoDB)TanmayMondkar.py

--- (MongoDB)TanmayMondkar.py
+++ (MongoDB)TanmayMondkar.py
@@ -12,13 +12,13 @@ db = client.test
 countries = db.countries
 continents = db.continents
 
-###   Get all the countries where the letter or the word is in the name   ###
+###   Function to get all the countries where the letter or the word is in the name   ###
 def search_by_query(name):
-    for con in countries.find({'name': {"$regex": name, "$options": 'i'}}):
-        print(con['name'])
+    for c in countries.find({'name': {"$regex": name, "$options": 'i'}}):
+        print(c['name'])
 
 
-###   Send list of continents with their number of countries   ###
+###   Function to send list of continents with their number of countries   ###
 def all_continents_and_countries():
     agg_pipeline = [
         {
@@ -27,34 +27,34 @@ def all_continents_and_countries():
                 'countries': {'$size': "$countries"}}
         }
     ]
-    cont = continents.aggregate(agg_pipeline)
-    for continent in cont:
+    conti = continents.aggregate(agg_pipeline)
+    for continent in conti:
         print(continent)
 
 
-###   Send back the fourth countries of a continent by alphabetical order   ###
+###   Function to send back the fourth countries of a continent by alphabetical order   ###
 def fourth_country():
     for continent in continents.find():
         for country_id in continent['countries']:
            countr = countries.find({'_id': ObjectId(country_id)}).sort("name")
            print(continent['name'],':',countr[0]['name'])
 
-###   Get all countries ordered by the number of people   ###
+###   Function to get all countries ordered by the number of people   ###
 def order_by_population():
-    for con in countries.find({}).sort("population"):
-        print("The country name is", con['name'], con['population'])
+    for cont in countries.find({}).sort("population"):
+        print("The country name is", cont['name'], cont['population'])
 
 
 
-###   Get countries which have 'u' in their name and population greater than 100 000   ###
+###   Function to get countries which have 'u' in their name and population greater than 100 000   ###
 def search_by_name_and_order_by_population():
-    for con in countries.find(
+    for cont in countries.find(
             {
                 'name':
                     {'$regex': 'u', '$options': 'i'}, 'population': {'$gt': 100000}
             }
             ).sort("population"):
-        print("The country name is", con['name'], con['population'])
+        print("The country name is", cont['name'], cont['population'])
 
 if __name__ == '__main__:':
     search_by_query('Fr')  
@@ -62,3 +62,4 @@ if __name__ == '__main__:':
     order_by_population()
     fourth_country()
     search_by_name_and_order_by_population()
+
